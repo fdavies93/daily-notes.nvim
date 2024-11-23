316301c57f31d83a250fa2ae2f6ab912e49eae40
Frank Davies <fdavies93@gmail.com>
2024-11-23 18:45:39 +0800

    Improve timestamp parsing and add date periods

--- lua/daily-notes/fuzzy-time.lua
+++ lua/daily-notes/fuzzy-time.lua
@@ -12,42 +12,81 @@ local weekdays = {
 	"sunday"
 }
 
--- TODO: Make this a config option
-local timestamp_formats = {
-	"%Y-%m-%d",
+local periods = {
+	"day",
+	"week"
 }
 
 local relative_days = {
 	today = function()
-		return time_plus_days(0)
+		return { time_plus_days(0), "day" }
 	end,
 	tomorrow = function()
-		return time_plus_days(1)
+		return { time_plus_days(1), "day" }
 	end,
 	yesterday = function()
-		return time_plus_days(-1)
+		return { time_plus_days(-1), "day" }
 	end,
 }
 
-local get_single_token_date = function(token)
+
+local get_single_token_date = function(token, opts)
 	local fn
 	if relative_days[token] ~= nil then
 		fn = relative_days[token]
 		return fn()
 	end
-	for i = 1, #timestamp_formats do
-		local timestamp = vim.fn.strptime(timestamp_formats[i], token)
-		if timestamp ~= 0 then
-			return timestamp
-		end
-	end
 	return nil
 end
 
-local get_date = function(input_string)
+local get_relative2 = function(tokens, opts)
+	local offset
+	if tokens[1] == "this" then
+		offset = 0
+	elseif tokens[1] == "next" then
+		offset = 1
+	elseif tokens[1] == "last" then
+		offset = -1
+	end
+
+	-- weekdays
+	for i = 1, #weekdays do
+
+	end
+end
+
+local get_two_token_date = function(tokens, opts)
+	if tokens[1] == "next" or tokens[1] == "last" or tokens[1] == "this" then
+		for i = 1, #periods do
+			if periods[i] == tokens[2] then
+				return get_relative2(tokens, opts)
+			end
+		end
+	end
+end
+
+local get_date = function(input_string, opts)
 	local tokens = vim.split(input_string, " ")
+	local timestamp_periods = opts.timestamp_formats
+	local timestamp = nil
 	if #tokens == 1 then
-		return get_single_token_date(tokens[1])
+		timestamp = get_single_token_date(tokens[1], opts)
+	elseif #tokens == 2 then
+		timestamp = get_two_token_date(tokens[2], opts)
+	end
+
+	if timestamp ~= nil then
+		return timestamp
+	end
+
+	-- we use the whole string to allow for multi-word date formats
+	for period, formats in pairs(timestamp_periods) do
+		for format = 1, #formats do
+			timestamp = vim.fn.strptime(timestamp_periods[period][format], input_string)
+			if timestamp ~= 0 then
+				return { timestamp, period }
+			end
+		end
 	end
 	return nil
 end
--- tests/fuzzy_time_spec.lua
+++ tests/fuzzy_time_spec.lua
@@ -1,34 +1,77 @@
 require("daily-notes").setup()
 local fuzzy_time = require("daily-notes.fuzzy-time")
+local opts = {
+	timestamp_formats = {
+		day = {
+			"%Y-%m-%d",
+			"%A, %B %d %Y"
+		},
+		week = {
+			"%Y Week %W"
+		}
+	},
+	week_starts = "monday",
+}
 
-describe('Fuzzy Time', function()
+describe('Fuzzy Time:', function()
 	it('Should return the correct date for today', function()
-		assert.equals(
-			fuzzy_time.get_date("today"),
-			os.time()
+		assert.same(
+			fuzzy_time.get_date("today", opts),
+			{
+				os.time(),
+				"day"
+			}
 		)
 	end)
 	it('Should return the correct date for tomorrow', function()
-		assert.equals(
-			fuzzy_time.get_date("tomorrow"),
-			os.time() + (24 * 60 * 60)
+		assert.same(
+			fuzzy_time.get_date("tomorrow", opts),
+			{
+				os.time() + (24 * 60 * 60),
+				"day"
+			}
 		)
 	end)
 	it('Should return the correct date for yesterday', function()
-		assert.equals(
-			fuzzy_time.get_date("yesterday"),
-			os.time() - (24 * 60 * 60)
+		assert.same(
+			fuzzy_time.get_date("yesterday", opts),
+			{
+				os.time() - (24 * 60 * 60),
+				"day"
+			}
 		)
 	end)
 	it('Should return the correct date from timestamp', function()
-		assert.equals(
-			fuzzy_time.get_date("2024-10-01"),
-			vim.fn.strptime("%Y-%m-%d", "2024-10-01")
+		assert.same(
+			fuzzy_time.get_date("2024-10-01", opts),
+			{
+				vim.fn.strptime("%Y-%m-%d", "2024-10-01"),
+				"day"
+			}
 		)
 	end)
+	it('Should return the correct date from multi-word timestamp', function()
+		assert.same(
+			fuzzy_time.get_date("Friday, November 01 2024", opts),
+			{
+				vim.fn.strptime("%A, %B %d %Y", "Friday, November 01 2024"),
+				"day"
+			}
+		)
+	end)
+	it('Should return a week type from a week format', function()
+		assert.same(
+			fuzzy_time.get_date("2024 Week 24", opts),
+			{
+				vim.fn.strptime("%Y Week %W", "2024 Week 24"),
+				"week"
+			}
+		)
+	end)
+
 	it('Should return nil from incorrect timestamp', function()
-		assert.equals(
-			fuzzy_time.get_date("2024/10/01"),
+		assert.same(
+			fuzzy_time.get_date("2024/10/01", opts),
 			nil
 		)
 	end)
